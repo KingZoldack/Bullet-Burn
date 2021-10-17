dac50e5612e64e539db66b1a05321c5056ccf17a
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-17 10:29:01 +0100

    Raycast
    Added Raycasting (weapon) script.

---
+++ Assets/Weapons/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera _FPCamera;
+    [SerializeField] float _range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(_FPCamera.transform.position, _FPCamera.transform.forward, out hit, _range);
+        Debug.Log($"{hit.transform.name} was shot at.");
+
+    }
+}
