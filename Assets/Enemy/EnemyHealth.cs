e18e10dd26c80b9a728f2d76cef30ee9464c24cd
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-17 11:19:44 +0100

    Enemy
    Implemented enemy health system.

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public static EnemyHealth instance;
+
+    [SerializeField] float _maxHealth = 100f;
+
+    float _currentHealth;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        _currentHealth = _maxHealth;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float Damage)
+    {
+        _currentHealth -= Damage;
+
+        if (_currentHealth < 1f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Weapons/Weapon.cs
+++ Assets/Weapons/Weapon.cs
@@ -7,6 +7,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera _FPCamera;
     [SerializeField] float _range = 100f;
+    [SerializeField] float _clipperBulletDamage = 20f;
 
     void Update()
     {
@@ -19,8 +20,17 @@ public class Weapon : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(_FPCamera.transform.position, _FPCamera.transform.forward, out hit, _range);
-        Debug.Log($"{hit.transform.name} was shot at.");
+        if (Physics.Raycast(_FPCamera.transform.position, _FPCamera.transform.forward, out hit, _range))
+        {
+            Debug.Log($"{hit.transform.name} was shot at.");
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(_clipperBulletDamage);
+        }
 
+        else
+        {
+            return;
+        }
     }
 }
