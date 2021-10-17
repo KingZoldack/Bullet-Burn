29427a57d8ff027a6d1baaad904f2235bc1fca82
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-17 08:51:11 +0100

    Initial Commit
    Objects added to scene as well as player and enemy.
    
    Added Nav mesh for enemy. Coded Enemy movement to always follow player.

---
+++ Assets/Enemy/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    [SerializeField] float _chaseRange = 5f;
+
+    NavMeshAgent _navMeshAgent;
+
+    float _distanceToTarget = Mathf.Infinity;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.SetDestination(_target.position);
+    }
+}
