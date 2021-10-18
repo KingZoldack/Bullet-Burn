997892f8f36320b4e7570ec576bd58fe9c9edb4d
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-19 00:29:30 +0100

    Weapon Switching
    Added keypad number select for individual weapons as well as scroll wheel functionality to switch.

--- Assets/Prefabs/Weapons/WeaponSwitcher.cs
+++ Assets/Prefabs/Weapons/WeaponSwitcher.cs
@@ -13,6 +13,69 @@ public class WeaponSwitcher : MonoBehaviour
         SetWeaponActive();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        int previousWeapon = _currentWeapon;
+
+        ProcessScrollWheel();
+        ProcessKeyInput();
+
+        if (previousWeapon != _currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (_currentWeapon >= transform.childCount - 1)
+            {
+                _currentWeapon = 0;
+            }
+
+            else
+            {
+                _currentWeapon++;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (_currentWeapon <= 0)
+            {
+                _currentWeapon = transform.childCount - 1;
+            }
+
+            else
+            {
+                _currentWeapon--;
+            }
+        }
+
+
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _currentWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _currentWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _currentWeapon = 2;
+        }
+    }
+
     void SetWeaponActive()
     {
         int weaponIndex = 0;
@@ -33,9 +96,5 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
